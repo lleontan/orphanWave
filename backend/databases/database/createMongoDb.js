87714f1de2cbe0d1a7070244bfc8f7e80874efc9
lleontan <lleontan@gmail.com>
2020-04-07 22:43:22 -0700

    Added dummy data script to mongo db

--- backend/databases/database/createMongoDb.js
+++ backend/databases/database/createMongoDb.js
@@ -1,18 +1,124 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+let constants = require('../../constants');
+let apiFetch = require("../../apiFetch");
+let MongoClient = require('mongodb').MongoClient;
+const schedule = require('node-schedule');
+//import schedule from 'node-schedule';
+
+const DATA_DB_URL = constants.DATA_DB_URL;
+const DATA_DB_NAME= constants.DATA_DB_NAME;
+const GENERIC_ERROR_MESSAGE = "Error Occured";
+const IEX_NOT_AVAILABLE_MESSAGE = "IEX Not active";
 
 
+const cpiMonthlyCollection={
+  name:"us_cpi_monthly",
+  route:"data-points/market/CPIAUCSL"
+};
+const unemploymentRateWeekly={
+  name:"unemployment_rate_weekly",
+  route:'time-series/economic/UNRATE'
+};
+const gdpGrowthQuarterly={
+  name:"gdp_growth_quarterly",
+  route:"todo"
+};
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("orphanWaveIndicators");
-  dbo.createCollection("us_cpi_monthly", function(err, res) {
-    if (err) throw err;
-    console.log("Mongo cpi_weekly collection created!");
-    //db.close();
+
+async function addCollection(collectionData,dbo){
+  /*console.log("Attempting drop"+ collectionData.name);
+  dbo.dropCollection(collectionData.name,(err,result)=>{
+    if (err) console.log(err);
+    if (result) console.log("Collection deleted");
+  });*/
+  //let collectionName=collectionData.name;
+  return new Promise(function(resolve, reject) {
+    let collectionName=collectionData.name;
+    console.log("Attempting add "+collectionName);
+    dbo.createCollection(collectionName, function(err, res) {
+      //if (err) throw err;
+      if (err){
+        console.log("error occurred in dbo.createCollection createMongoDb.js");
+        console.log(err);
+        reject();
+      }
+      console.log("Mongo "+collectionName+" collection created!");
+      resolve();
+    });
+  })
+}
+
+
+//Returns the date string
+function getNewestDateFromCollection(collection){
+  return collection.find().sort({"releaseDate": -1}).limit(1);
+}
+
+
+//This function queries IEX for new cpi and quarterly gdp growth
+//If the new data is more recent than the old data it is inserted into the collection
+//With a daily update check this should add identical datapoints for every day in the time period.
+async function updateTimeSeriesFromIex(collectionData,dbo) {
+  const data=await apiFetch.fetchIexText(collectionData.route).then(data => {
+      let collection = dbo.collection(collectionData.name);
+      let newestDateInCollectionString=getNewestDateFromCollection(collection);
+      let previousNewestDate=new Date(newestDateInCollectionString);
+      let newFoundDate=new Date(data);
+      if(newFoundDate>previousNewestDate){
+        console.log("Found that "+data+" was more recent than "+newestDateInCollectionString+", adding new date.");
+        collection.insertOne({
+          actual:data,
+          releaseDate:Date.now()
+        }, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+        });
+      }
   });
-  dbo.createCollection("gdp_growth_quarterly", function(err, res) {
-    if (err) throw err;
-    console.log("Mongo us_gdp_monthly collection created!");
+}
+
+
+function scheduleIexUpdates() {
+  schedule.scheduleJob('0 0 * * *', () => {
+    console.log("Scheduled Iex update started at " + Date.now());
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db(DATA_DB_NAME);
+      updateTimeSeriesFromIex(unemploymentRateWeekly,dbo);
+      updateTimeSeriesFromIex(cpiMonthlyCollection, dbo);
+      updateTimeSeriesFromIex(gdpGrowthQuarterly,dbo);
+      db.close();
+    });
   });
-});
+};
+
+async function addDefaultCollections(dbo){
+  return new Promise(async function(resolve, reject) {
+    await addCollection(cpiMonthlyCollection,dbo);
+    await addCollection(gdpGrowthQuarterly,dbo);
+    await addCollection(unemploymentRateWeekly,dbo);
+    resolve() // successfully fill promise
+  })
+}
+
+async function onStart() {
+  console.log(DATA_DB_URL);
+
+  MongoClient.connect(DATA_DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, async function(err, db) {
+        try{
+          if (err) throw err;
+          console.log("Connected to mongo database");
+          var dbo = db.db(DATA_DB_NAME);
+          await addDefaultCollections(dbo);
+        }catch(error){
+          throw Error(error);
+        }finally{
+          db.close();
+        }
+      }
+  );
+  //scheduleIexUpdates();
+}
+onStart();
--- backend/databases/database/populateWithDummyData.js
+++ backend/databases/database/populateWithDummyData.js
@@ -1,32 +1,62 @@
+let constants = require('../../constants');
+
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
-
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("orphanWaveIndicators");
-  var dummyGdpGrowthQuarterly = [
-    { actual: 1.1, release_date: new Date("2019-03-18"), note:"Actual Value"},
-    { actual: 3.1, release_date: new Date("2019-06-18"), note:"Actual Value"},
-    { actual: 2, release_date: new Date("2019-09-18"), note:"Actual Value"},
-    { actual: 2.1, release_date: new Date("2019-12-20"), note:"Actual Value"},
-    { actual: 2.1, release_date: new Date("2020-03-26"), note:"Actual Value"}
-  ];
-  var dummyCpiInflationRateMonthly = [
-      { actual: 2.3, release_date: new Date("2019-12-11"), note:"Dummy Value"},
-      { actual: 2.3, release_date: new Date("2020-01-14"), note:"Dummy Value"},
-      { actual: 2.3, release_date: new Date("2020-02-13	"), note:"Dummy Value"},
-      { actual: 2.4, release_date: new Date("2020-03-11"), note:"Dummy Value"}
-  ];
-  dbo.collection("gdp_growth_quarterly").insertMany(dummyGdpGrowthQuarterly, function(err, res) {
+var url = constants.DATA_DB_URL;
+const DB_NAME=constants.DATA_DB_NAME;
+const dummyGdpGrowthQuarterly = [
+  { actual: 1.1, releaseDate: new Date("2019-03-18"), note:"Actual Value"},
+  { actual: 3.1, releaseDate: new Date("2019-06-18"), note:"Actual Value"},
+  { actual: 2, releaseDate: new Date("2019-09-18"), note:"Actual Value"},
+  { actual: 2.1, releaseDate: new Date("2019-12-20"), note:"Actual Value"},
+  { actual: 2.1, releaseDate: new Date("2020-03-26"), note:"Actual Value"}
+];
+const dummyCpiInflationRateMonthly = [
+    { actual: 2.3, releaseDate: new Date("2019-12-11"), note:"Actual Value"},
+    { actual: 2.3, releaseDate: new Date("2020-01-14"), note:"Actual Value"},
+    { actual: 2.3, releaseDate: new Date("2020-02-13	"), note:"Actual Value"},
+    { actual: 2.4, releaseDate: new Date("2020-03-11"), note:"Actual Value"}
+];
+const dummyUnemploymentRateWeekly = [
+    { actual: 2.3, releaseDate: new Date("2019-12-11"), note:"Dummy Value"},
+    { actual: 2.3, releaseDate: new Date("2020-01-14"), note:"Dummy Value"},
+    { actual: 2.3, releaseDate: new Date("2020-02-13	"), note:"Dummy Value"},
+    { actual: 2.4, releaseDate: new Date("2020-03-11"), note:"Dummy Value"}
+];
+function insertDummyGdpGrowthQuarterly(){
+  insertDummyData("gdp_growth_quarterly",dummyGdpGrowthQuarterly,true);
+}
+function insertDummyCpiInflationRateMonthly(){
+  insertDummyData("us_cpi_monthly",dummyCpiInflationRateMonthly,true);
+}
+function insertDummyUnemploymentRateWeekly(){
+  insertDummyData("unemployment_rate_weekly",dummyUnemploymentRateWeekly,true);
+}
+function insertDummyData(collectionName,newData,dropOldData){
+  console.log("Mongo url is "+url);
+  MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log("Number of documents inserted: " + res.insertedCount);
-    db.close();
+    var dbo = db.db(DB_NAME);
+    let collection = dbo.collection(collectionName);
+    console.log("Inserting "+newData.length+" datapoints to "+collectionName);
+    if(dropOldData){
+      console.log("Preexisting data dropped before insert");
+      collection.drop();
+    }
+    collection.insertMany(newData, function(err, res) {
+      if (err) throw err;
+      console.log("Number of documents inserted to "+collectionName+": " + res.insertedCount);
+      collection.countDocuments().then((count) => {
+          console.log("New count is "+count);
+          db.close();
+      });
+    });
   });
-  dbo.collection("us_cpi_monthly").insertMany(dummyCpiInflationRateMonthly, function(err, res) {
-    if (err) throw err;
-    console.log("Number of documents inserted: " + res.insertedCount);
-    db.close();
-  });
-});
+}
+
+function onStart(){
+  console.log("Populating mongo db with default data. Most dummy data is real though.");
+  insertDummyGdpGrowthQuarterly();
+  insertDummyCpiInflationRateMonthly();
+  insertDummyUnemploymentRateWeekly();
+}
+onStart();
